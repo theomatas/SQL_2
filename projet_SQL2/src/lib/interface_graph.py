575fa4dd421d9092a79020500b40c73ef5450a69
theomatas <32847860+theomatas@users.noreply.github.com>
2018-12-07 05:49:34 +0100

    Add files via upload

--- projet_SQL2/src/lib/interface_graph.py
+++ projet_SQL2/src/lib/interface_graph.py
@@ -6,8 +6,10 @@ class new_fen ():
               self.canvas = canvas              
               self.COM = 0
               self.bouton = []
-              self.fen_1()
               self.BDD = BDD
+              self.table_set()
+              self.fen_1()
+              
               
        def fen_1(self):
               self.clean()
@@ -30,15 +32,38 @@ class new_fen ():
                             pass
               self.bouton = bouton
        def consulter(self):
-              self.COM = 1
+              if not self.BDD:
+                     return self.info()
+              self.clean()
+              bouton = []
+              canvas = self.canvas
+              fen = self.fen
+              patryck = PhotoImage(file= "../image/binary.gif")
+              bouton.append(canvas.create_image(300, 300, image = patryck))
+              canvas.image = patryck       
+              bouton.append(canvas.create_rectangle( 25,25,575,490,  fill = "silver" ))
+              bouton.append(Button(fen, text="Retour", command=self.fen_1,bg="orangered",font=('Helvetica', '10'), width = 25, height = 3))
+              for i in range(len(bouton)):
+                     try:
+                            bouton[i].place(x = 175 , y = i*85 + 340 )
+                     except:
+                            pass
+
+              for i in range(len(self.table)):
+                     bouton.append(Button(fen, text=str(self.table[i][0]), command= lambda i=i: self.select(i), bg="khaki",font=('Helvetica', '10'), width = 15, height = 2))
+                     bouton[-1].pack()
+                     bouton[-1].place(x = 50 + 180 * (i%3)  , y = i//3*70 + 100 )
+
+              self.bouton = bouton
        def editer(self):
-              self.COM = 2
+              if not self.BDD:
+                     return self.info()
        def clean(self):
               for i in self.bouton:
                      try:
                             self.canvas.delete(i)
                      except:
-                            i.destroy()                      
+                            i.destroy() 
        def info(self):
               self.clean()
               bouton = []
@@ -48,13 +73,13 @@ class new_fen ():
               bouton.append(canvas.create_image(300, 300, image = patryck))
               canvas.image = patryck       
               bouton.append(canvas.create_rectangle( 25,25,575,490,  fill = "silver" ))
-              bouton.append(Button(fen, text="Fermer", command=self.fen_1,bg="orangered",font=('Helvetica', '10'), width = 25, height = 3))
+              bouton.append(Button(fen, text="Retour", command=self.fen_1,bg="orangered",font=('Helvetica', '10'), width = 25, height = 3))
               txt = "Bienvenue sur notre application de gestion \nde planning.\n\nvous pouvez ici ajouter ou consulter les\ninformation contenue sur une BBD \
 simplifiee\nsimulant celle d'une ecole.\n\n* cree par:\n - Theo Matas\n - Jean Panthou \n - Hanaa\n\n"
               if self.BDD:
                      txt += "BDD prete et charger"
               else:
-                     txt += "BDD non trouve WAMP est-il allume"
+                     txt += "BDD non trouve WAMP est-il allume ?"
               txt = txt.split('\n')
               for i in range(len(txt)):
                      bouton.append(canvas.create_text( 35, i*30 + 50 , text = txt[i] , font=('Helvetica', '15'), fill = "red" ,  anchor= 'w'))
@@ -66,3 +91,38 @@ simplifiee\nsimulant celle d'une ecole.\n\n* cree par:\n - Theo Matas\n - Jean P
               self.bouton = bouton
        def get_COM(self):
               return self.COM
+       
+       def table_set(self):
+              table = []
+              if self.BDD:
+                     for tables in self.BDD.request_line("SHOW TABLES;"):
+                            clmn_lst = []
+                            for clmn in  self.BDD.request_line("SHOW COLUMNS FROM " + tables[0] + ";"):
+                                   clmn_lst.append(clmn[0])
+ 
+                            
+                            table.append([tables[0],clmn_lst])
+                                   
+              self.table = table      
+       def select(self,num):
+              self.clean()
+              bouton = []
+              canvas = self.canvas
+              fen = self.fen
+              patryck = PhotoImage(file= "../image/binary.gif")
+              bouton.append(canvas.create_image(300, 300, image = patryck))
+              canvas.image = patryck       
+              bouton.append(canvas.create_rectangle( 25,25,575,490,  fill = "silver" ))
+              bouton.append(Button(fen, text="Retour", command=self.consulter,bg="orangered",font=('Helvetica', '10'), width = 25, height = 3))
+              for i in range(len(bouton)):
+                     try:
+                            bouton[i].place(x = 175 , y = i*85 + 340 )
+                     except:
+                            pass
+
+              for i in range(len(self.table[num][1])):
+                     bouton.append(Button(fen, text=str(self.table[num][1][i]), bg="khaki",font=('Helvetica', '10'), width = 15, height = 2))
+                     bouton[-1].pack()
+                     bouton[-1].place(x = 50 + 180 * (i%3)  , y = i//3*70 + 100 )
+
+              self.bouton = bouton
--- projet_SQL2/src/main.py
+++ projet_SQL2/src/main.py
@@ -1,9 +1,10 @@
 import lib.simple_fonction as X
 import lib.creat_base as B
-import lib.sql_fonction as S
+import lib.sql_fonction as SQL
 import lib.interface_script as I
 import lib.interface_graph as G
 from tkinter import * 
+
 
 def init():
     global fen, canvas, Base, ACT
@@ -17,14 +18,24 @@ def init():
         STATE CHAR(2), 
         LAT_N REAL, 
         LONG_W REAL);
+        CREATE TABLE STAT 
+        (ID INTEGER PRIMARY KEY, 
+        CITY CHAR(20), 
+        STATE CHAR(2), 
+        LAT_N REAL, 
+        LONG_W REAL);
         INSERT INTO STATION VALUES (13, 'Phoenix', 'AZ', 33, 112);
         INSERT INTO STATION VALUES (44, 'Denver', 'CO', 40, 105);
         INSERT INTO STATION VALUES (66, 'Caribou', 'ME', 47, 68);
+        SHOW COLUMNS FROM station;
+        SHOW TABLES;
         """
         text = Base.request(cmd)
-        ACT = G.new_fen(fen,canvas,Button,True)
+        print(text)
+        ACT = G.new_fen(fen,canvas,Button,Base)
     except:
         ACT = G.new_fen(fen,canvas,Button, False)
+    #print(effecteur())
     loop()
     
     
@@ -32,20 +43,20 @@ def init():
 def effecteur():
     res = I.listerner()
     if res[0] == 0:
-        cmd = S.selector(res[1][0],res[1][1])
+        
+        cmd = SQL.selector(res[1][0],res[1][1])
     if res[0] == 1:
-        cmd = S.insertor(res[1][0],res[1][1])
+        cmd = SQL.insertor(res[1][0],res[1][1])
     print (cmd)
     text = Base.request_line(cmd)
     return text
 
 def loop():
-    print(ACT.get_COM())
     fen.after(100,loop)
 
 
 
-#print(effecteur())
+
 init()
 canvas.pack()
 fen.mainloop()  
